95805a8e6efcccad9211d7e579b6e084069f9af8
Shannon Beach <shannonbeach@Shannons-MBP.attlocal.net>
2017-01-01 19:53:32 -0500

    Added addIngredient function and ability to add measurements

--- classes/recipe.php
+++ classes/recipe.php
@@ -1,13 +1,56 @@
-<? php
+<?php
 
 class Recipe
 {
-    public $title;
+    private $title;
     public $ingredients = array();
     public $instructions = array();
     public $yeild;
     public $tag = array();
     public $source  = "Alena Holligan";
+
+    private $measurements = array(
+        'tsp',
+        'tbsp',
+        'cup',
+        'oz',
+        'lb',
+        'fl oz',
+        'pint',
+        'quart',
+        'gallon'
+    );
+
+    public function setTitle($title)
+    {
+        $this->title = ucwords($title);
+        //this line sets the value of the title property to the value that was passed as an
+        //argument
+        //$this->title = access to the title property
+        //the past arguement =$title;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function addIngredient($item, $amount = null, $measure = null)
+    {
+        if($amount != null && !is_float($amount) && !is_int($amount)){
+            exit("The amount must be a float: " . gettype($amount) . " amount given.");
+        }
+
+        if($measure != null && !in_array(strtolower($measure), $this->measurements)){
+            exit("Please enter a vaild measurement: " . implode(",", $this->measurements));
+        }
+            $this->ingredients[] = array(
+              'item' => ucwords($item),
+              'amount' => $amount,
+              'measure' => strtolower($measure)
+            );
+
+    }
 
     public function displayRecipe()
     {
@@ -17,14 +60,20 @@ class Recipe
 
 $recipe1 = new Recipe();
 
-//referece the class properties
-echo $recipe1 -> source;
+//referece the class properties ->
 $recipe1 -> source = "Shannon Beach";
+$recipe1-> setTitle = ("my first recipe");
+$recipe1-> addIngredient("egg", 1, "doz");
 
 $recipe2 = new Recipe();
 $recipe2 -> source = "Betty Crocker";
+$recipe2-> setTitle = ("My Second Recipe");
 
-var_dump($recipe1);
+//access class methods ->
+echo $recipe1->getTitle();
+echo $recipe1->displayRecipe();
+echo $recipe2->displayRecipe();
+
 
 
 
