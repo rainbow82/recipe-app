3c320f94424256d6e57f38bee187639743117f97
Shannon Beach <shannonbeach@Shannons-MBP.attlocal.net>
2017-01-02 20:10:49 -0500

    Created cookbook "Treehouse Recipes"

--- cookbook.php
+++ cookbook.php
@@ -2,7 +2,28 @@
 
 include "classes/recipe.php";
 include "classes/render.php";
+include "classes/RecipeCollection.php";
 include "inc/allrecipes.php";
+
+$cookbook = new RecipeCollection("Treehouse Recipes");
+$cookbook->addRecipe($lemon_chicken);
+$cookbook->addRecipe($granola_muffins);
+$cookbook->addRecipe($belgian_waffles);
+$cookbook->addRecipe($pepper_casserole);
+$cookbook->addRecipe($lasagna);
+$cookbook->addRecipe($dried_mushroom_ragout);
+$cookbook->addRecipe($rabbit_catalan);
+$cookbook->addRecipe($grilled_salmon_with_fennel);
+$cookbook->addRecipe($pistachio_duck);
+$cookbook->addRecipe($chili_pork);
+$cookbook->addRecipe($crab_cakes);
+$cookbook->addRecipe($beef_medallions);
+$cookbook->addRecipe($silver_dollar_cakes);
+$cookbook->addRecipe($french_toast);
+$cookbook->addRecipe($corn_beef_hash);
+$cookbook->addRecipe($granola);
+$cookbook->addRecipe($spicy_omelette);
+$cookbook->addRecipe($scones);
 
 // $recipe1 = new Recipe("my first recipe");
 
